c78e9eca7f837c7cceba4666b7c8d56121f048c0
gaohailang <gaohailang@wandoujia.com>
2014-05-06 04:19:52 +0800

    polish: wtf of the base and script not at the same context?

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-cat manifest.js dependency/underscore.js dependency/jquery.min.js dependency/uikit.js dependency/keymaster.js engine.js > dist-base.js
-# cat sites/*.js
-echo "" > dist-scripts.js
+cat manifest.js > dist-base.js
+echo "\n" >> dist-base.js
+cat engine.js >> dist-base.js
+cat dependency/underscore.js dependency/jquery.min.js dependency/keymaster.js engine.js > dist-scripts.js
+echo "\n" >> dist-scripts.js
 cd sites; for each in *.js; do cat $each >> ../dist-scripts.js; echo "\n" >> ../dist-scripts.js; done
