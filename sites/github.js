17740792131d7a07f19f32c30171fbdeb3646c50
HailangGao <gaohailang@wandoujia.com>
2014-05-16 13:26:33 +0800

    polish: github blob setInterval to check because its pjax way to load new page

--- sites/github.js
+++ sites/github.js
@@ -2,15 +2,24 @@ addRoute('github.com', '.*/blob/.*', function() {
     // code src page
     console.log('github.js is running');
 
-    addCopySrcBtn();
+    // addCopySrcBtn
+    (function() {
 
-    function addCopySrcBtn() {
-        if ($('#addCopySrcBtn').length) return;
-        var copySrcBtn = $('<button id="addCopySrcBtn" aria-label="copy to clipboard" class="js-zeroclipboard minibutton zeroclipboard-button" data-clipboard-text="#" data-copied-hint="copied!" type="button"><span class="octicon octicon-clippy"></span></button>').appendTo('.actions .button-group');
-        var codeSrcText = '';
-        $('.code-body .line').each(function(idx, item) {
-            codeSrcText += $(item).text() + '\n'
-        });
-        copySrcBtn.attr('data-clipboard-text', codeSrcText);
-    }
+        function _addCopySrcBtn() {
+            var copySrcBtn = $('<button aria-label="copy to clipboard" class="js-copySrcBtn js-zeroclipboard minibutton zeroclipboard-button" data-clipboard-text="#" data-copied-hint="copied!" type="button"><span class="octicon octicon-clippy"></span></button>').appendTo('.actions .button-group');
+            var codeSrcText = '';
+            $('.code-body .line').each(function(idx, item) {
+                codeSrcText += $(item).text() + '\n'
+            });
+            copySrcBtn.attr('data-clipboard-text', codeSrcText);
+        }
+
+        // GitHub doesn't reload the page but uses pjax. Need to detect and load Octotree.
+        function detectLocationChange() {
+            if ($('.js-copySrcBtn').length) return;
+            _addCopySrcBtn();
+        }
+        window.setInterval(detectLocationChange, 200);
+        detectLocationChange();
+    })();
 });
